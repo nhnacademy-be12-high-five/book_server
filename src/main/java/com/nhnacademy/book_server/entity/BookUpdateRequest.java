f5801c589ef03221c7aae54d0ae07538ebcc9ac2
hjStack <minnie1498@naver.com>
2025-12-02 09:45:04 +0900

    ✨ feat: 관리자 테스트 코드 작성

--- src/main/java/com/nhnacademy/book_server/entity/BookUpdateRequest.java
+++ src/main/java/com/nhnacademy/book_server/entity/BookUpdateRequest.java
@@ -1,4 +1,20 @@
 package com.nhnacademy.book_server.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class BookUpdateRequest {
+    private String title;
+    private Integer price;
+    @NotBlank(message = "ISBN은 필수 입력 값입니다.")
+    private String isbn;
+
+    private String publisher;       // 출판사
+    private LocalDate publishedDate; // 출판일
+    private String description;      // 책 소개/요약
 }
