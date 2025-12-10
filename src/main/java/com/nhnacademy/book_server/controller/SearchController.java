1b46f27f37208b24a7c1159b753e04c3f86ce332
ryutaehun <fbxogns321@naver.com>
2025-12-10 15:34:15 +0900

    feat:Search api 경로 수정(간단)

--- src/main/java/com/nhnacademy/book_server/controller/SearchController.java
+++ src/main/java/com/nhnacademy/book_server/controller/SearchController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("api/search")
+@RequestMapping("/api/search")
 @RequiredArgsConstructor
 public class SearchController implements SearchSwagger {
 
