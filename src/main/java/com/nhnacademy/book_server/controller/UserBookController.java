ab776fb2b519833d3d9869ad7f4e587ab67e80b0
songdohyeok <songdh801@naver.com>
2025-12-13 16:29:56 +0900

    feat: 주문에서 필요한 api 임시 추가

--- src/main/java/com/nhnacademy/book_server/controller/UserBookController.java
+++ src/main/java/com/nhnacademy/book_server/controller/UserBookController.java
@@ -88,4 +88,15 @@ public class UserBookController implements UserBookSwagger {
         bookService.updateWeeklyRanking();
         return ResponseEntity.ok("주간 랭킹 집계 완료!");
     }
+
+
+    // Order Server가 호출하는 api/books/{bookId}/stock/hold
+    @PostMapping("/books/{bookId}/stock/hold")
+    public ResponseEntity<Void> holdStock(@PathVariable Long bookId,
+                                          @RequestParam Integer quantity,
+                                          @RequestHeader("Idempotency-Key") String idempotencyKey) {
+        bookService.holdStock(bookId, quantity);
+
+        return ResponseEntity.ok().build();
+    }
 }
