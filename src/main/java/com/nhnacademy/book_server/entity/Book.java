eaa0fb50051453612ce42e40c9c131cb40c8f22d
TAEHUN <fbxogns321@naver.com>
2025-12-26 09:36:45 +0900

    :sparkles: feat:검색 리팩토링 작업

--- src/main/java/com/nhnacademy/book_server/entity/Book.java
+++ src/main/java/com/nhnacademy/book_server/entity/Book.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.BatchSize;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +43,7 @@ public class Book {
     @NotNull
     @Builder.Default
     @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true) // todo
+    @BatchSize(size = 100)
     private List<BookAuthor> bookAuthors = new ArrayList<>(); // List 초기화는 @Builder에서 처리됨
     // AUTHR_NM : 저자이름
     // 도서와 저자는 1:N 관계 -> 한권의 책에 여러 저자가 있을 수 있음
@@ -101,6 +104,7 @@ public class Book {
 
     @Builder.Default
     @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    @BatchSize(size = 100)
     private List<BookCategory> bookCategories = new ArrayList<>();
     //  도서와 카테고리는 1:N관계
 
@@ -120,5 +124,6 @@ public class Book {
     }
 
     @OneToMany(mappedBy = "book", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @BatchSize(size = 100)
     private List<BookTag> bookTags = new ArrayList<>();
 }
