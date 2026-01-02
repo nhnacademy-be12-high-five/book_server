69944cf1d9fc3fa38ed7a4a642a1eb73ea97b883
hjStack <minnie1498@naver.com>
2026-01-02 22:18:09 +0900

    feat: 코드 커버리지 높이기

--- src/main/java/com/nhnacademy/book_server/service/BookService.java
+++ src/main/java/com/nhnacademy/book_server/service/BookService.java
@@ -441,7 +441,9 @@ public class BookService {
         // 존재 여부 확인 후 삭제
         if (bookLikeRepository.existsByBook_IdAndMemberId(bookId, memberId)) {
             bookLikeRepository.deleteByBook_IdAndMemberId(bookId, memberId);
-        } else {
+        }
+
+        else {
             throw new RuntimeException("삭제할 좋아요 기록이 없습니다.");
         }
     }
